3dbb895aa0b104a0615e4965295990236148f573
paulportillo <yummta@gmail.com>
2020-03-22 03:08:17 -0500

    Update new enpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ import SocketContext from "./contexts/SocketContext";
 import io from "socket.io-client";
 
 function App() {
-  const ENDPOINT = "https://cow-bull-server.herokuapp.com/";
+  const ENDPOINT = "https://cow-bull.herokuapp.com/";
   // const ENDPOINT = "http://localhost:8000/";
   const [socket, setSocket] = useState("");
 
